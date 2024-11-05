26c9c72ed5a6577d44d40320f1ba144c52121735
Shivam <shivamagarwalmarch@gmail.com>
2024-11-05 23:00:54 +1100

    Updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Die from './components/Die';
 import Button from './components/Button';
 
 export default function App() {
+
+  function generateRandomDiceNumbers(){
+    return(
+      Array(10).fill(null).map(() => Math.floor(Math.random() * 6) + 1)
+    );
+  }
+
+  
   return(
     <main>
       <Header />
-      <Die value={1}/>
+      <section className='dice-container'>
+          {generateRandomDiceNumbers().map(number => (
+            <Die 
+              value={number}
+            />
+          ))}
+      </section>
       <Button />
     </main>
   );
--- src/components/Die.js
+++ src/components/Die.js
@@ -2,17 +2,10 @@ import React from 'react';
 
 export default function Die(props){
   return(
-    <section className='dice-container'>
-      <div className='die'><span className="die-num">{props.value}</span></div>
-      <div className='die'><span className="die-num">{props.value}</span></div>
-      <div className='die'><span className="die-num">{props.value}</span></div>
-      <div className='die'><span className="die-num">{props.value}</span></div>
-      <div className='die'><span className="die-num">{props.value}</span></div>
-      <div className='die'><span className="die-num">{props.value}</span></div>
-      <div className='die'><span className="die-num">{props.value}</span></div>
-      <div className='die'><span className="die-num">{props.value}</span></div>
-      <div className='die'><span className="die-num">{props.value}</span></div>
-      <div className='die'><span className="die-num">{props.value}</span></div>
-    </section>
+      <div className='die'>
+        <span className="die-num">
+          {props.value}
+        </span>
+      </div>
   );
 }
